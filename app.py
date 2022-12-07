f760b11ef0df2f4bae65a0b5c3fa89fe11c47f35
eriklungulescu <eriklungulescu@outlook.com>
2022-12-07 16:23:53 -0500

    added logging and error handling for exceptions caused my user input

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import logging
 from flask import Flask
 from controllers.controller import session
 from example import run_machine
@@ -12,5 +13,7 @@ def hello_name():
     return "Server is healthy!"
  
 if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s [%(levelname)s] :: %(message)s ', level=logging.DEBUG)
+    logging.getLogger("websockets").setLevel(logging.CRITICAL)
     run_machine()
     # app.run()

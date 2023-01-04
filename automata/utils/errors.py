da3eb3c3b5f5278ca5703fe66420c70b99515aa1
eriklungulescu <71273428+eriklungulescu@users.noreply.github.com>
2023-01-04 14:32:07 -0500

    allow errors to propogate up stack trace for a clearer understanding of runtime expections (#4)

--- automata/utils/errors.py
+++ automata/utils/errors.py
@@ -1,4 +1,7 @@
-# Input Errors
+'''
+    Automata Design Errors
+    - Occurs when a user attempts to create an invalid state machine design
+'''
 
 class DuplicateInputIdentifierBaseError(Exception):
     """
@@ -31,12 +34,31 @@ class DuplicateStateError(DuplicateInputIdentifierBaseError):
     def __str__(self):
         return super().__str__() + f" -> tried adding duplicate state {self._state} to machine {self._machine}" 
 
-# Automata Errors
+'''
+    Client Runtime Errors
+    - Errors that occur when a client request is invalid
+'''
 
-class InvalidStateTransition:
+class InvalidStateTransition(Exception):
     def __init__(self, state: str, target: str):
         self._state = state
         self._target = target 
 
     def __str__(self):
         return f"Tried an invalid state transition from {self._state} to {self._target}"
+
+class InvalidPayload(Exception):
+    def __init__(self, payload: str):
+        self._invalid_payload = payload 
+
+    def __str__(self):
+        return f"Tried to process an invalid payload: {self._invalid_payload}"
+
+class InvalidEvent(Exception):
+    def __init__(self, current_state: str, invalid_event: str, allowed_events: str):
+        self._current_state = current_state
+        self._invalid_event = invalid_event
+        self._allowed_events = allowed_events
+
+    def __str__(self):
+        return f"Tried to call an invalid event: {self._invalid_event}. The allowed events for the state {self._current_state} are {self._allowed_events}"
